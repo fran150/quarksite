8f4d829639c1aa0788966359b83cb5fac14bd80f
admin <fllopez@bpba.com.ar>
2016-08-11 18:00:24 -0300

    Testing layout components

--- components/test.js
+++ components/test.js
@@ -1,12 +1,5 @@
 define(['quark', 'knockout', 'text!./test.html'], function($$, ko, template) {
     return $$.component(function(params, $scope, $imports) {
         var self = this;
-
-        $imports.initComponent = function() {
-            debugger;
-            var test = $$.routing.link("route:/#main/components/library/module|{ bowerId: 'test' }");
-            self.componentErrors.add('Error de prueba');
-
-        }
     }, template);
 });
--- pages/navbar.js
+++
@@ -1,5 +0,0 @@
-define(['quark', 'knockout', 'text!./navbar.html'], function($$, ko, template) {
-    return $$.component(function(params, $scope, $imports) {
-        var self = this;
-    }, template);
-});
