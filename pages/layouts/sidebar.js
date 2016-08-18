08640c7485da6f9538fdfaf6d314068b3d362cfb
admin <fllopez@bpba.com.ar>
2016-08-18 18:03:03 -0300

    Make use of show binding

--- pages/layouts/sidebar.js
+++ pages/layouts/sidebar.js
@@ -1,5 +1,7 @@
-define(['quark', 'knockout', 'text!./sidebar.html', 'text!./navbar.html'], function($$, ko, template, navbar) {
+define(['quark', 'knockout', 'text!./sidebar.html', 'text!./navbar.html'], function($$, ko, template, navbarHTML) {
     return $$.component(function(params, $scope, $imports) {
         var self = this;
+
+        $scope.navbarHtml = ko.observable(navbarHTML);
     }, template);
 });
--- pages/layouts/single.js
+++ pages/layouts/single.js
@@ -1,5 +1,7 @@
-define(['quark', 'knockout', 'text!./single.html'], function($$, ko, template) {
+define(['quark', 'knockout', 'text!./single.html', 'text!./navbar.html'], function($$, ko, template, navbarHTML) {
     return $$.component(function(params, $scope, $imports) {
         var self = this;
+
+        $scope.navbarHtml = ko.observable(navbarHTML);
     }, template);
 });
