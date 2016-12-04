8d694c5108fb3b9574e712b6b9b67e3db31f77aa
fran150 <franllopez@yahoo.com.ar>
2016-12-04 20:30:14 -0300

    Moved qk-alchemy to use src directory

--- app/require.config.js
+++ app/require.config.js
@@ -6,7 +6,7 @@ require = requireConfigure(require, {
         'data':             'data',
         'bootstrap':        'bower_components/bootstrap/dist/js/bootstrap.min',
         'qk-bootstrap':     'bower_components/qk-bootstrap',
-        'qk-alchemy':       'bower_components/qk-alchemy',
+        'qk-alchemy':       'bower_components/qk-alchemy/src',
         'prism':            'bower_components/prism/prism'
     },
     shim: {
